1fb1c60657550d319355a0ec8ba2628c178121be
David <44948149+klaricd@users.noreply.github.com>
2020-05-04 13:37:05 +0200

    Platformer

---
+++ Assets/Skripte/PlatformFalling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFalling : MonoBehaviour
+{
+    public float fallDelay = 1f;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb.isKinematic = false;
+    }
+}
---
+++ Assets/Skripte/SimplePlatformController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlatformController : MonoBehaviour
+{
+    [HideInInspector] public bool moveRight = true;
+    [HideInInspector] public bool jump = true;
+
+    public float moveSpeed = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool onGround = false;
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        
+        // no jumping in air
+        if(Input.GetButton("Jump") && onGround)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb.velocity.x < maxSpeed)
+            rb.AddForce(Vector2.right * h * moveSpeed);
+
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+
+        if (h > 0 && !moveRight)
+            Rotate();
+        else if (h < 0 && moveRight)
+            Rotate();
+
+        if (jump)
+        {
+            animator.SetTrigger("Jump");
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+
+    }
+
+    void Rotate()
+    {
+        moveRight = !moveRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
